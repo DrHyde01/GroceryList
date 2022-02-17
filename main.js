03e2995d98575efac52fda23d57b23601b592ace
Hyde Stefan <79136747+DrHyde01@users.noreply.github.com>
2022-02-17 19:16:17 +0100

    JS in progress

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let addInput = document.querySelector(".add__input");
 let addBtn = document.querySelector(".add__btn");
 let itemList = document.querySelector(".items__list");
+let delBtn = document.querySelector(".delete__btn");
 
 // Localstorage
 let toStorage = () =>
@@ -10,15 +11,26 @@ let fromStorage = () => JSON.parse(localStorage.getItem("Shopping List")) || [];
 const itemStore = fromStorage();
 
 addBtn.addEventListener("click", () => {
-  let inputValue = addInput.value;
-  itemStore.push(inputValue);
+  itemStore.push(addInput.value);
   toStorage();
-  alert(`${inputValue} a été rajouté`);
+  //alert(`${addInput.value} a été rajouté`);
   addInput.value = "";
+  showItems(); // To show new items on click
+});
+
+delBtn.addEventListener("click", () => {
+  deleteItems();
 });
 
 const showItems = () => {
+  itemList.innerHTML = ""; //Show nothing as default
   for (item of itemStore) {
-    itemList.innerHTML += `<li>${item}</li>`;
+    itemList.innerHTML += `<li class="items__elm">${item}</li>`;
   }
 };
+
+const deleteItems = () => {
+  localStorage.clear();
+  location.reload();
+};
+showItems();
